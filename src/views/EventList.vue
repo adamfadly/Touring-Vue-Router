c8950fca09474c53549f10030f552ba38ef4942e
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-06-21 18:00:26 +0700

    rerite tag component

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>Events for Good</h1>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <event-card
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></event-card>
   </div>
 </template>
 
